c2eb32779c0d03448677cfaa63cceb324d667295
mehmutyix <mehmutyix@gmail.com>
2020-04-24 12:52:57 -0700

    add SecondGitPush.java

---
+++ src/day_01/SecondGitPush.java
@@ -0,0 +1,9 @@
+package day_01;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class SecondGitPush {
+    public static void main(String[] args) {
+        System.out.println("This time i am trying to test push...");
+    }
+}
