46e293944ee9b88930d3380126bde7b3ffa9b8ff
maihemutijiang <60989189+mehmutyix@users.noreply.github.com>
2020-04-28 12:42:21 -0700

    Update HappyBirthday.java

--- src/day_01/HappyBirthday.java
+++ src/day_01/HappyBirthday.java
@@ -3,5 +3,6 @@ package day_01;
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy Birthday to you");
+        // I made change on github and i will push it to local repo
     }
 }
