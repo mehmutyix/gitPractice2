7a9e21e5fcd51647016eedd066200a2fc4a3cff1
maihemutijiang <60989189+mehmutyix@users.noreply.github.com>
2020-04-28 12:59:11 -0700

    Update HappyBirthday.java

--- src/day_01/HappyBirthday.java
+++ src/day_01/HappyBirthday.java
@@ -4,5 +4,6 @@ public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy Birthday to you");
         // I made change on github and i will push it to local repo
+        // I am trying git fetch function
     }
 }
