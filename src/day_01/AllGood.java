720ff0f32ec18661684d5fbb6abce4faef83a06b
mehmutyix <mehmutyix@gmail.com>
2020-04-28 12:35:32 -0700

    update AllGood.java

--- src/day_01/AllGood.java
+++ src/day_01/AllGood.java
@@ -5,5 +5,7 @@ public class AllGood {
 
         // I hope everything will be good.
         // This line is to try edit from github
+        // i made changes today 4/28/2020 in softskill class
+        // im excited to learn new things
     }
 }
